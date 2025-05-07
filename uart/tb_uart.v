9c04098c9a6573df43f08e95c2ded9e4b6f34428
Jun-robot <saijun721.1@gmail.com>
2025-05-07 19:08:48 +0900

    add uart_tx

---
+++ uart/tb_uart.v
@@ -0,0 +1,42 @@
+module tb_uart;
+  reg clk;
+  reg rst;
+  reg start;
+  reg [7:0] data_in;
+  wire tx;
+  wire busy;
+
+  uart_tx uut (
+    .clk(clk),
+    .rst(rst),
+    .start(start),
+    .data_in(data_in),
+    .tx(tx),
+    .busy(busy)
+  );
+
+  initial begin
+    $dumpfile(`DUMPFILE);
+    $dumpvars(0, tb_uart);
+  end
+
+  initial begin
+    clk = 0;
+    forever #2.5 clk = ~clk;
+  end
+
+  initial begin
+    rst = 1;
+    start = 0;
+    data_in = 8'b00000011;
+
+    #10 rst = 0; // Release reset after some time
+    #5 start = 1; // Start transmission
+    #5 start = 0; // Clear start signal
+
+    #100; // Wait for transmission to complete
+
+    $finish;
+  end
+
+endmodule
---
+++ uart/uart_tx.v
@@ -0,0 +1,55 @@
+module uart_tx(
+  input wire clk,
+  input wire rst,
+  input wire start,
+  input wire [7:0] data_in,
+  output reg tx,
+  output reg busy
+);
+
+localparam IDLE = 2'b00;
+localparam START = 2'b01;
+localparam DATA = 2'b10;
+localparam STOP = 2'b11;
+
+reg [1:0] state;
+reg [3:0] bit_count;
+reg [7:0] shift_reg;
+
+
+always @(posedge clk or posedge rst) begin
+  busy = (state != IDLE);
+  if(rst) begin
+    state <= IDLE;
+    tx <= 1'b1;
+    busy <= 1'b0;
+  end else begin
+    case(state)
+      IDLE: begin
+        if(start) begin // 1bit遅れてスタート
+          state <= START;
+          bit_count <= 4'b0000;
+          shift_reg <= data_in;
+        end
+      end
+      START: begin
+        state <= DATA;
+        tx <= 1'b0; // Start bit
+      end
+      DATA: begin
+        tx <= shift_reg[0]; // Transmit LSB first
+        shift_reg <= {1'b0, shift_reg[7:1]}; // Shift left
+        if(bit_count == 4'b0111) begin
+          state <= STOP;
+        end else begin
+          bit_count <= bit_count + 1;
+        end
+      end
+      STOP: begin
+        tx <= 1'b1; // Stop bit
+        state <= IDLE;
+      end
+    endcase
+  end
+end
+endmodule
